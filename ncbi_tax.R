0d3c8fa966907d7d4196677f59195a09cecbd30d
Francesco Beghini <francesco.beghini@unitn.it>
2019-04-10 23:21:35 +0200

    simple script to navigate ncbi taxonomy

--- ncbi_tax.R
+++ ncbi_tax.R
@@ -1,6 +1,40 @@
-ncbi_taxonomy_dump_url <- 'ftp://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz'
-download.file(ncbi_taxonomy_dump_url, '/shares/CIBIO-Storage/CM/scratch/users/francesco.beghini/taxdump.tar.gz')
-untar('/shares/CIBIO-Storage/CM/scratch/users/francesco.beghini/taxdump.tar.gz', exdir = '/shares/CIBIO-Storage/CM/scratch/users/francesco.beghini/taxdump_190410')
-names_buf <- read.delim('/shares/CIBIO-Storage/CM/scratch/users/francesco.beghini/taxdump_190410/names.dmp', header = FALSE, sep = '\t')
-nodes_buf <- read.delim('/shares/CIBIO-Storage/CM/scratch/users/francesco.beghini/taxdump_190410/nodes.dmp', header = FALSE, sep = '\t')
+ncbi_taxonomy_dump_url <-
+  'https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/new_taxdump/new_taxdump.tar.gz'
 
+download.file(
+  ncbi_taxonomy_dump_url,
+  '/shares/CIBIO-Storage/CM/scratch/users/francesco.beghini/taxdump.tar.gz'
+)
+
+untar(
+  '/shares/CIBIO-Storage/CM/scratch/users/francesco.beghini/taxdump.tar.gz',
+  exdir = '/shares/CIBIO-Storage/CM/scratch/users/francesco.beghini/taxdump_190410'
+)
+
+taxonomy_buf <-
+  scan(
+    '/shares/CIBIO-Storage/CM/scratch/users/francesco.beghini/taxdump_190410/rankedlineage.dmp',
+    sep = "\t",
+    what = "raw",
+    quote = NULL
+  )
+
+taxonomy <-
+  matrix(taxonomy_buf, ncol = 20, byrow = TRUE)[, c(TRUE, FALSE)]
+
+taxonomy <- as.data.frame(taxonomy)
+
+colnames(taxonomy) <-
+  c(
+    "tax_id",
+    "tax_name",
+    "species",
+    "genus",
+    "family",
+    "order",
+    "class",
+    "phylum",
+    "kingdom",
+    "superkingdom"
+  )
+taxonomy %>% select(tax_id == 1280)

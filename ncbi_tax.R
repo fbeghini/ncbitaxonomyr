af99cf790ae909f3f585d8d42947ec61f902d44d
Francesco Beghini <francesco.beghini@unitn.it>
2019-04-13 12:15:17 +0200

    export

--- ncbi_tax.R
+++ ncbi_tax.R
@@ -1,3 +1,6 @@
+library(magrittr)
+library(dplyr)
+
 ncbi_taxonomy_dump_url <-
   'https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/new_taxdump/new_taxdump.tar.gz'
 
@@ -37,4 +40,48 @@ colnames(taxonomy) <-
     "kingdom",
     "superkingdom"
   )
-taxonomy %>% select(tax_id == 1280)
+# taxonomy %>% select(tax_id == 1280)
+taxonomy %<>% dplyr::filter(taxonomy$superkingdom != '') %>% select(-kingdom) %>% mutate_all(funs(stringr::str_replace_all(., ' ', '_')))
+taxonomy %<>% select(superkingdom, phylum, class, order, family, genus, species) %>% distinct()
+
+taxonomy$superkingdom <- paste0('k__', taxonomy$superkingdom)
+taxonomy$phylum <- paste0('p__', taxonomy$phylum)
+taxonomy$class <- paste0('c__', taxonomy$class)
+taxonomy$order <- paste0('o__', taxonomy$order)
+taxonomy$family <- paste0('f__', taxonomy$family)
+taxonomy$genus <- paste0('g__', taxonomy$genus)
+taxonomy$species <- paste0('s__', taxonomy$species)
+
+taxonomy %<>% mutate_all(funs(stringr::str_replace_all(. , '[kpocfgs]__$', '')))
+taxlvl <- 'kpocfgs'
+
+mpa_taxa <- apply(head(taxonomy), 1, function(x) {
+  empty_lvls <- which(x == "")
+  if (!tail(empty_lvls, 1) - head(empty_lvls, 1) == length(empty_lvls) -
+      1) {
+    has_species <- x['species'] != ""
+    last_taxa_known <- head(empty_lvls, 1) - 1
+    x[empty_lvls] <-
+      sapply(empty_lvls, function(idx)
+        paste0(
+          stringr::str_sub(taxlvl, idx, idx),
+          '__',
+          stringr::str_sub(x[last_taxa_known], 4),
+          '_unclassified'
+        ))
+    if (!has_species)
+      x['species'] <- NA
+  } else {
+    x[empty_lvls] <- NA
+  }
+  x
+}) %>% t %>% as.data.frame %>% tidyr::unite('mpa_tax',
+                                            superkingdom,
+                                            phylum,
+                                            class,
+                                            order,
+                                            family,
+                                            genus,
+                                            species,
+                                            sep = '|') %>%
+  dplyr::mutate(stringr::str_replace(mpa_tax, stringr::fixed('|NA'), ''))
